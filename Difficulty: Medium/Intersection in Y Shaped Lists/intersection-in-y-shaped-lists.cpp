ee0448cd3f4b7fa19b5885332a41ef5f373aa83e
kanurisravanthi <144417416+sravanthi-sys@users.noreply.github.com>
2026-01-01 14:49:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.cpp
@@ -0,0 +1,32 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* intersectPoint(Node* head1, Node* head2) {
+        Node* t1=head1;
+        Node* t2=head2;
+        while(t1!=t2){
+            t1=t1->next;
+           t2=t2->next;
+           if(t1==NULL){
+               t1=head2;
+           }
+           if(t2==NULL){
+               t2=head1;
+           }
+       }
+       return t1;
+        
+    }
+};
